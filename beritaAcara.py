9dd757e69d256f119b0acc3c502a4cd4f98dcced
rizalmurtadha <m.rizalmurtadha@gmail.com>
2020-07-27 15:53:33 +0900

    fitur cari

--- beritaAcara.py
+++ beritaAcara.py
@@ -126,9 +126,9 @@ def index():
                     filename_pdf = "Sidang_"+NIM+".pdf"
                     headers_filename = "inline; filename="+filename_pdf
                     css = ["static/css/bootstrap.min.css","static/style.css"]
-                    # config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
-                    # pdf = pdfkit.from_string(html, False,configuration=config, css=css)
-                    pdf = pdfkit.from_string(html, False, css=css)
+                    config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
+                    pdf = pdfkit.from_string(html, False,configuration=config, css=css)
+                    # pdf = pdfkit.from_string(html, False, css=css)
                     response = make_response(pdf)
                     response.headers["Content-Type"] = "application/pdf"
                     response.headers["Content-Disposition"] = headers_filename
